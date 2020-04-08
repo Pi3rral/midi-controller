2947ea49c32edef753f3cfe94c65a35097e0ccf5
Pi3rral <pierre.leca@alayacare.com>
2020-04-07 21:16:24 -0400

    MIDI message struct

--- midi-controller/MIDIController.cpp
+++ midi-controller/MIDIController.cpp
@@ -7,20 +7,24 @@ void MIDIController::init(Stream* _stream, uint8_t _default_channel) {
     this->default_channel = _default_channel;
 }
 
-void MIDIController::sendMIDI(uint8_t messageType, uint8_t channel, uint8_t data1, uint8_t data2) {
+void MIDIController::sendMIDI(uint8_t message_type, uint8_t channel, uint8_t data1, uint8_t data2) {
     channel--; // Decrement the channel, because MIDI channel 1 corresponds to binary channel 0
-    messageType &= 0b11110000; // Make sure that only the high nibble of the message type is set
+    message_type &= 0b11110000; // Make sure that only the high nibble of the message type is set
     channel &= 0b00001111; // Make sure that only the low nibble of the channel is set
-    uint8_t statusByte = messageType | channel; // Combine the messageType (high nibble)
+    uint8_t status_byte = message_type | channel; // Combine the message_type (high nibble)
     // with the channel (low nibble)
     // Both the message type and the channel
     // should be 4 bits wide
-    statusByte |= 0b10000000; // Set the most significant bit of the status byte
+    status_byte |= 0b10000000; // Set the most significant bit of the status byte
     data1 &= 0b01111111; // Clear the most significant bit of the data bytes
     data2 &= 0b01111111;
-    this->stream->write(statusByte); // Send over Serial
+    this->stream->write(status_byte); // Send over Serial
     this->stream->write(data1);
     this->stream->write(data2);
+}
+
+void MIDIController::sendMIDIMessage(MIDIMessage message) {
+    this->sendMIDI(message.message_type, message.channel, message.data1, message.data2);
 }
 
 void MIDIController::sendProgramChange(uint8_t program_number, uint8_t channel) {
