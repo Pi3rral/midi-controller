0906af38c58f48234a7e33798e4e0d5c5fe8769b
Pi3rral <pierre.leca@alayacare.com>
2021-07-22 11:48:52 -0400

    fix baudrate

--- esp8266/src/midicontroller/midi/ports.py
+++ esp8266/src/midicontroller/midi/ports.py
@@ -3,7 +3,7 @@ from machine import UART
 
 class ESP8266TX1Port:
     def __init__(self) -> None:
-        self.port = UART(1, baudrate=32500)
+        self.port = UART(1, baudrate=31250)
 
     def write(self, packet, num) -> None:
         self.port.write(packet)
