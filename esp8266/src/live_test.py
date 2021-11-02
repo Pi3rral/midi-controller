8e0ca628927ade583f114a65416dfb2d59c6fc7a
Pi3rral <pierre.leca@alayacare.com>
2021-11-02 11:11:55 -0400

    improve test live file

--- esp8266/src/live_test.py
+++ esp8266/src/live_test.py
@@ -13,24 +13,68 @@ midi = MIDI(midi_out=midi_port)
 MidiPort.midi_object = midi
 
 
-plumes_on = Action(
-    "midi",
-    {"type": "control_change", "channel": "6", "control": "10", "value": "1"},
-)
-plumes_off = Action(
-    "midi",
-    {"type": "control_change", "channel": "6", "control": "10", "value": "0"},
-)
-print("sleep 2s")
-sleep_ms(2000)
-print("Plumes On")
-plumes_on.do_action()
-print("sleep 3s")
-sleep_ms(3000)
-print("Plumes Off")
-plumes_off.do_action()
-sleep_ms(3000)
-print("Plumes On")
-plumes_on.do_action()
-print("sleep 2s")
-sleep_ms(2000)
+def the_imp_change_boot(channel, n):
+    print("------------------------")
+    print("change boot on channel " + str(channel))
+    message_1 = Action(
+        "midi",
+        {"type": "control_change", "channel": channel, "control": "8", "value": n},
+    )
+    message_2 = Action(
+        "midi",
+        {"type": "control_change", "channel": channel, "control": "9", "value": "18"},
+    )
+    message_3 = Action(
+        "midi",
+        {"type": "control_change", "channel": channel, "control": "9", "value": "52"},
+    )
+    message_4 = Action(
+        "midi",
+        {"type": "control_change", "channel": channel, "control": "9", "value": "0"},
+    )
+    message_1.do_action()
+    sleep_ms(100)
+    message_2.do_action()
+    sleep_ms(100)
+    message_3.do_action()
+    sleep_ms(100)
+    message_4.do_action()
+    sleep_ms(100)
+    print("done")
+    print("------------------------")
+
+
+def test_on_channel(channel):
+    print("------------------------")
+    print("test channel " + str(channel))
+    pedal_on = Action(
+        "midi",
+        {"type": "control_change", "channel": channel, "control": "10", "value": "1"},
+    )
+    pedal_off = Action(
+        "midi",
+        {"type": "control_change", "channel": channel, "control": "10", "value": "0"},
+    )
+    print("sleep 1s")
+    sleep_ms(1000)
+    print("Pedal On")
+    pedal_on.do_action()
+    print("sleep 2s")
+    sleep_ms(2000)
+    print("Pedal Off")
+    pedal_off.do_action()
+    print("sleep 2s")
+    sleep_ms(2000)
+    print("Pedal On")
+    pedal_on.do_action()
+    print("sleep 1s")
+    sleep_ms(1000)
+    print("------------------------")
+
+
+test_on_channel(0)
+# the_imp_change_boot(5, 30)
+
+
+# for i in range(0, 16):
+#     test_on_channel(i)
